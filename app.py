52c0a6b6fcd86758b7236acc3923dc902e5b13a2
Vishnu Siva B <vs21official@gmail.com>
2024-03-31 14:49:36 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, redirect, url_for, request
+from pymongo import MongoClient
+
+app = Flask(__name__)
+uri = "mongodb+srv://Vishnusiva:vishnusiva@cluster0.fcphozc.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri)
+db = client["flask_database"]
+todos = db["todos"]
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        todo = request.form['todo']
+        degree = request.form['degree']
+        todos.insert_one({"todo": todo, "degree": degree})
+        return redirect(url_for("index"))
+    
+    all_todos = list(todos.find())
+    return render_template("index.html", all_todos=all_todos)
+
+@app.route("/del/<name>",methods=['GET', 'POST'])
+def delete(name):
+    todos.delete_one({"todo":name})
+    return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    app.run(debug=1)
